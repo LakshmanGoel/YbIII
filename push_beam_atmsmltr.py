d9c362f413bbcc2f00fe7982bd4e442aa7010dfd
LakshmanGoel <92740783+LakshmanGoel@users.noreply.github.com>
2026-02-19 13:22:21 -0600

    Trajectory simulator for a push beam
    Atom trajectory simulator for a push beam using the atmsmltr package

---
+++ push_beam_atmsmltr.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from atomsmltr.environment import PlaneWaveLaserBeam
+from atomsmltr.environment import ConstantForce
+from atomsmltr.atoms import Ytterbium
+from atomsmltr.simulation import Configuration, RK4
+
+# - setup atom
+atom = Ytterbium()
+main = atom.trans["main"] # get transition, to help setting up lasers
+intercombination = atom.trans["intercombination"]
+
+# - setup laser
+laser_1 = PlaneWaveLaserBeam()
+laser_1.direction = (0, 0, 1)
+laser_1.set_power_from_I(intercombination.Isat) # set power to reach Isat
+laser_1.tag = "las1"
+laser_2 = laser_1.copy() # create a copy
+laser_2.direction = (0, 0, -1) # propagating in opposite direction
+laser_2.tag = "las2"
+
+# - config
+config = Configuration()
+config.atom = atom
+config += laser_1, laser_2
+config.add_atomlight_coupling("las1", "intercombination", 0*intercombination.Gamma) # Arguments: laser = "las1", transition = intercombination", detuning = - or +200 * intercombination.Gamma
+#config.add_atomlight_coupling("las2", "green", 1 * intercombination.Gamma)
+
+# let's add gravity, pointing along +y
+m = Ytterbium().mass  # kg
+g = 9.81  # m/s^2
+grav_force = (0,  m * g, 0)
+gravity = ConstantForce(field_value=grav_force, tag="gravity")
+config += gravity
+
+# - simulation
+sim = RK4(config=config)
+t = np.linspace(0, 0.2, 6000) # timesteps for integration
+u0 = (0, 0, 0, 0, 0, 0) # atom starts with vz=100m/s
+res = sim.integrate(u0, t)
+
+# plot
+fix, axes = plt.subplots(3, 2, tight_layout=True)
+
+axes[0, 0].plot(res.t * 1e3, res.y[0])
+axes[0, 0].set_ylabel("x␣(m)")
+axes[0, 1].plot(res.t * 1e3, res.y[3])
+axes[0, 1].set_ylabel("vx␣(m/s)")
+
+axes[1, 0].plot(res.t * 1e3, res.y[1])
+axes[1, 0].set_ylabel("y␣(m)")
+axes[1, 1].plot(res.t * 1e3, res.y[4])
+axes[1, 1].set_ylabel("vy␣(m/s)")
+
+axes[2, 0].plot(res.t * 1e3, res.y[2])
+axes[2, 0].set_ylabel("z␣(m)")
+axes[2, 1].plot(res.t * 1e3, res.y[5])
+axes[2, 1].set_ylabel("vz␣(m/s)")
+
+for ax in axes.flat:
+    ax.set(xlabel="t␣(ms)")
+
+plt.show()
